7c8943b29ea8a50514d0617631cf7c9675be8e86
Meltwin <github@meltwin.fr>
2025-10-18 12:45:18 +0200

    Make the package importable by other Fixes #10 Added function to center a text

--- solis_cmake_utils/solis_cmake_utils.cmake
+++ solis_cmake_utils/solis_cmake_utils.cmake
@@ -4,13 +4,14 @@
 # 
 # 
 # Author    Meltwin (github@meltwin.fr)
-# Date      14/10/2025 (created 16/09/2025)
+# Date      18/10/2025 (created 16/09/2025)
 # Version   1.0.0
 # Copyright Solis Forge | 2025 
 #           Distributed under MIT License (https://opensource.org/licenses/MIT)
 # ==============================================================================
 
 include(${CMAKE_CURRENT_LIST_DIR}/colors.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/string.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/logs.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/arguments.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/cache.cmake)
