57889a6eab21aec1102e77c0346a4bd367cdbcb7
Meltwin <github@meltwin.fr>
2025-11-08 20:05:39 +0100

    Added script registration

--- solis_cmake_targets/solis_cmake_targets.cmake
+++ solis_cmake_targets/solis_cmake_targets.cmake
@@ -4,7 +4,7 @@
 # Entry-point of the targets-related CMake module
 # 
 # Author    Meltwin (github@meltwin.fr)
-# Date      19/10/2025 (created 18/10/2025)
+# Date      08/11/2025 (created 19/10/2025)
 # Version   1.0.0
 # Copyright Solis Forge | 2025 
 #           Distributed under MIT License (https://opensource.org/licenses/MIT)
@@ -13,3 +13,4 @@
 include("${CMAKE_CURRENT_LIST_DIR}/targets_cmake.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/targets_cxx.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/targets_python.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/targets_scripts.cmake")
